6284da02800a6f56895d7d9f01f68255b7883e4a
OpenShift guest <krzychukula@gmail.com>
2013-04-23 08:27:44 -0400

    formatting

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
+/*global createjs:true */
 
 //Create a stage by getting a reference to the canvas
-stage = new createjs.Stage('can');
+var stage = new createjs.Stage('can');
+
 //Create a Shape DisplayObject.
-circle = new createjs.Shape();
+var circle = new createjs.Shape();
 circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+
 //Set position of Shape instance.
 circle.x = circle.y = 50;
+
 //Add Shape instance to stage display list.
 stage.addChild(circle);
+
 //Update stage will render next frame
 stage.update();
+
+
 
 //Update stage will render next frame
 createjs.Ticker.addEventListener("tick", handleTick);
