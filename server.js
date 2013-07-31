491cbdd641e726ece98b02fadbf59297dd4018df
OpenShift guest <krzychukula@gmail.com>
2013-07-31 08:20:35 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+var ip = process.env.IP || 'localhost';
+app.listen(port, ip, function() {
+  console.log("Listening on " + ip + port);
+});
